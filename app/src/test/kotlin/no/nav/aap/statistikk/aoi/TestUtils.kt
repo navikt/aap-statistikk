9af3e2878b684ec7162e35847c6b1a046c72aa83
Fredrik Meyer <fredrik.meyer@nav.no>
2024-07-04 12:11:17 +0200

    Legg til postgres-test

--- app/src/test/kotlin/no/nav/aap/statistikk/aoi/TestUtils.kt
+++ app/src/test/kotlin/no/nav/aap/statistikk/aoi/TestUtils.kt
@@ -4,14 +4,21 @@ import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.testing.*
-import io.mockk.mockk
+import no.nav.aap.statistikk.DbConfig
 import no.nav.aap.statistikk.api.HendelsesRepository
 import no.nav.aap.statistikk.module
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 fun testKlient(hendelsesRepository: HendelsesRepository, test: suspend (HttpClient) -> Unit) {
+    val postgres = postgreSQLContainer()
+    val dbConfig = DbConfig(database = "sss", url = postgres.jdbcUrl, password = postgres.password, username = postgres.username)
+
     testApplication {
         application {
-            module(hendelsesRepository)
+            module(dbConfig, hendelsesRepository)
         }
         val client = client.config {
             install(ContentNegotiation) {
@@ -21,4 +28,11 @@ fun testKlient(hendelsesRepository: HendelsesRepository, test: suspend (HttpClie
 
         test(client)
     }
+}
+
+private fun postgreSQLContainer(): PostgreSQLContainer<Nothing> {
+    val postgres = PostgreSQLContainer<Nothing>("postgres:15")
+    postgres.waitingFor(HostPortWaitStrategy().withStartupTimeout(Duration.of(60L, ChronoUnit.SECONDS)))
+    postgres.start()
+    return postgres
 }
