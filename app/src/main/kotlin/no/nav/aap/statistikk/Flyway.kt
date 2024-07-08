7cdae2b9b7e51382db27aee98dd767046dda93c5
Fredrik Meyer <fredrik.meyer@nav.no>
2024-07-08 13:44:33 +0200

    Legg til flywway

---
+++ app/src/main/kotlin/no/nav/aap/statistikk/Flyway.kt
@@ -0,0 +1,28 @@
+package no.nav.aap.statistikk
+
+import com.zaxxer.hikari.HikariDataSource
+import org.flywaydb.core.Flyway
+import javax.sql.DataSource
+
+class Flyway {
+    fun createAndMigrateDataSource(config: DbConfig): DataSource {
+        val dataSource = HikariDataSource().apply {
+            jdbcUrl = config.url        // Database URL
+            username = config.username  // Database brukernavn
+            password = config.password  // Database passord
+        }
+
+        val flyway = Flyway
+            .configure()
+            //.cleanDisabled(miljø != MiljøKode.LOKALT)
+            //.cleanOnValidationError(miljø == MiljøKode.LOKALT)
+            .dataSource(dataSource)
+            .locations("migrering")
+            .validateMigrationNaming(true)
+            .load()
+
+        flyway.migrate()
+
+        return dataSource  // Returnerer den konfigurerte og migrerte datasourcen
+    }
+}
