5a669c84dab2a6435d29ccc11d21c777c877cc88
Fredrik Meyer <fredrik.meyer@nav.no>
2024-07-08 12:22:54 +0200

    Motta status og behandlingsType

--- app/src/main/kotlin/no/nav/aap/statistikk/api/MottaStatistikkDTO.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/api/MottaStatistikkDTO.kt
@@ -2,6 +2,8 @@ package no.nav.aap.statistikk.api
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class MottaStatistikkDTO(@JsonProperty(value = "sakId", required = true) val sakId: String) {
-
-}
+data class MottaStatistikkDTO(
+    @JsonProperty(value = "saksnummer", required = true) val saksNummer: String,
+    @JsonProperty(value = "status", required = true) val status: String,
+    @JsonProperty(value="behandlingsType", required = true) val behandlingsType: String,
+)
