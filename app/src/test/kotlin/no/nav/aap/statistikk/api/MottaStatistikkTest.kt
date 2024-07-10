95c31ed248f10391b07d246ae9034fd72055fd65
Fredrik Meyer <fredrik.meyer@nav.no>
2024-07-10 14:28:46 +0200

    Dummy-endepunkt for vilkarsresultat

--- app/src/test/kotlin/no/nav/aap/statistikk/api/MottaStatistikkTest.kt
+++ app/src/test/kotlin/no/nav/aap/statistikk/api/MottaStatistikkTest.kt
@@ -5,16 +5,17 @@ import io.ktor.http.*
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class MottaStatistikkTest {
     @Test
     fun `hendelse blir lagret i repository`() {
-        val IHendelsesRepository = mockk<IHendelsesRepository>()
-        every { IHendelsesRepository.lagreHendelse(any()) } returns Unit
+        val hendelsesRepository = mockk<IHendelsesRepository>()
+        every { hendelsesRepository.lagreHendelse(any()) } returns Unit
 
-        testKlient(IHendelsesRepository) { client ->
+        testKlient(hendelsesRepository) { client ->
             val res = client.post("/motta") {
                 contentType(ContentType.Application.Json)
                 setBody(
@@ -29,12 +30,44 @@ class MottaStatistikkTest {
         }
 
         verify {
-            IHendelsesRepository.lagreHendelse(
+            hendelsesRepository.lagreHendelse(
                 MottaStatistikkDTO(
                     saksNummer = "123", status = "OPPRETTET",
                     behandlingsType = "Førstegangsbehandling"
                 )
             )
+        }
+    }
+
+    @Test
+    fun `kan motta json av vilkårsresultat`() {
+        @Language("json")
+        val vilkårsResultatJSON = """{
+  "saksnummer" : "1234",
+  "typeBehandling" : "Førstegangsbehandling",
+  "vilkår" : [ {
+    "vilkårType" : "MEDLEMSKAP",
+    "perioder" : [ {
+      "fraDato" : "2024-07-09",
+      "tilDato" : "2024-07-11",
+      "utfall" : "OPPFYLT",
+      "manuellVurdering" : false,
+      "innvilgelsesårsak" : null,
+      "avslagsårsak" : null
+    } ]
+  } ]
+}"""
+
+        val hendelsesRepository = mockk<IHendelsesRepository>()
+
+        testKlient(hendelsesRepository) { client ->
+            val res = client.post("/vilkarsresultat") {
+                contentType(ContentType.Application.Json)
+                setBody(
+                    vilkårsResultatJSON
+                )
+            }
+            Assertions.assertEquals(HttpStatusCode.Accepted, res.status)
         }
     }
 }
