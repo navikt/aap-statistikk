5d8873513d944c9e3a2823422715e4e824189322
Fredrik Meyer <fredrik.meyer@nav.no>
2024-07-14 16:23:39 +0200

    Fiks import

--- app/src/main/kotlin/no/nav/aap/statistikk/App.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/App.kt
@@ -17,8 +17,8 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.micrometer.core.instrument.binder.logging.LogbackMetrics
-import io.micrometer.prometheus.PrometheusConfig
-import io.micrometer.prometheus.PrometheusMeterRegistry
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import no.nav.aap.statistikk.hendelser.api.MottaStatistikkDTO
 import no.nav.aap.statistikk.hendelser.api.mottaStatistikk
 import no.nav.aap.statistikk.bigquery.IBigQueryClient
