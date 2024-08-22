b2e881830e769def285325a1d058a264398995db
Fredrik Meyer <fredrik.meyer@nav.no>
2024-08-22 15:05:31 +0200

    Oppdater actions + legg på en param docstring

--- api-kontrakt/src/main/kotlin/no/nav/aap/statistikk/api_kontrakt/MottaStatistikkDTO.kt
+++ api-kontrakt/src/main/kotlin/no/nav/aap/statistikk/api_kontrakt/MottaStatistikkDTO.kt
@@ -4,6 +4,9 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
+/**
+ * @param saksnummer Saksnummer.
+ */
 data class MottaStatistikkDTO(
     val saksnummer: String,
     val behandlingReferanse: UUID,
