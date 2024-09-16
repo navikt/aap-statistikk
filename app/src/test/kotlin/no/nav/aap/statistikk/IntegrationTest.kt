7f6ee198e373594f71647a4d146a6fbe83a5204f
Fredrik Meyer <fredrik.meyer@nav.no>
2024-09-16 16:05:56 +0200

    Sjekk litt flere ting i integrasjonstest

--- app/src/test/kotlin/no/nav/aap/statistikk/IntegrationTest.kt
+++ app/src/test/kotlin/no/nav/aap/statistikk/IntegrationTest.kt
@@ -6,24 +6,6 @@ import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
-import no.nav.aap.statistikk.api_kontrakt.AvklaringsbehovHendelse
-import no.nav.aap.statistikk.api_kontrakt.AvsluttetBehandlingDTO
-import no.nav.aap.statistikk.api_kontrakt.BehovType
-import no.nav.aap.statistikk.api_kontrakt.BeregningsgrunnlagDTO
-import no.nav.aap.statistikk.api_kontrakt.Definisjon
-import no.nav.aap.statistikk.api_kontrakt.Endring
-import no.nav.aap.statistikk.api_kontrakt.EndringStatus
-import no.nav.aap.statistikk.api_kontrakt.Grunnlag11_19DTO
-import no.nav.aap.statistikk.api_kontrakt.GrunnlagYrkesskadeDTO
-import no.nav.aap.statistikk.api_kontrakt.MottaStatistikkDTO
-import no.nav.aap.statistikk.api_kontrakt.TilkjentYtelseDTO
-import no.nav.aap.statistikk.api_kontrakt.TilkjentYtelsePeriodeDTO
-import no.nav.aap.statistikk.api_kontrakt.TypeBehandling
-import no.nav.aap.statistikk.api_kontrakt.Utfall
-import no.nav.aap.statistikk.api_kontrakt.VilkårDTO
-import no.nav.aap.statistikk.api_kontrakt.VilkårsPeriodeDTO
-import no.nav.aap.statistikk.api_kontrakt.VilkårsResultatDTO
-import no.nav.aap.statistikk.api_kontrakt.Vilkårtype
 import no.nav.aap.statistikk.bigquery.BigQueryClient
 import no.nav.aap.statistikk.bigquery.BigQueryConfig
 import no.nav.aap.statistikk.bigquery.VilkårsVurderingTabell
@@ -34,13 +16,11 @@ import no.nav.aap.statistikk.testutils.Fakes
 import no.nav.aap.statistikk.testutils.Postgres
 import no.nav.aap.statistikk.testutils.TestToken
 import no.nav.aap.statistikk.testutils.avsluttetBehandlingDTO
+import no.nav.aap.statistikk.testutils.behandlingHendelse
 import no.nav.aap.statistikk.testutils.testKlientNoInjection
 import no.nav.aap.statistikk.testutils.ventPåSvar
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.util.UUID
 
 @Fakes
@@ -56,83 +36,7 @@ class IntegrationTest {
 
         val behandlingReferanse = UUID.randomUUID()
         val saksnummer = "4LFK2S0"
-        val hendelse = MottaStatistikkDTO(
-            saksnummer = saksnummer,
-            behandlingReferanse = behandlingReferanse,
-            status = "UTREDES",
-            behandlingType = TypeBehandling.Førstegangsbehandling,
-            ident = "14890097570",
-            avklaringsbehov = listOf(
-                AvklaringsbehovHendelse(
-                    definisjon = Definisjon(
-                        type = "5003",
-                        behovType = BehovType.valueOf("MANUELT_PÅKREVD"),
-                        løsesISteg = "AVKLAR_SYKDOM"
-                    ),
-                    status = EndringStatus.valueOf("SENDT_TILBAKE_FRA_KVALITETSSIKRER"),
-                    endringer = listOf(
-                        Endring(
-                            status = EndringStatus.valueOf("OPPRETTET"),
-                            tidsstempel = LocalDateTime.parse("2024-08-14T10:35:34.842"),
-                            frist = null,
-                            endretAv = "Kelvin"
-                        ),
-                        Endring(
-                            status = EndringStatus.valueOf("AVSLUTTET"),
-                            tidsstempel = LocalDateTime.parse("2024-08-14T11:50:50.217"),
-                            frist = null,
-                            endretAv = "Z994573"
-                        )
-                    )
-                ),
-                AvklaringsbehovHendelse(
-                    definisjon = Definisjon(
-                        type = "5006",
-                        behovType = BehovType.valueOf("MANUELT_PÅKREVD"),
-                        løsesISteg = "VURDER_BISTANDSBEHOV"
-                    ),
-                    status = EndringStatus.valueOf("SENDT_TILBAKE_FRA_KVALITETSSIKRER"),
-                    endringer = listOf(
-                        Endring(
-                            status = EndringStatus.valueOf("OPPRETTET"),
-                            tidsstempel = LocalDateTime.parse("2024-08-14T11:50:52.049"),
-                            frist = null,
-                            endretAv = "Kelvin"
-                        ),
-                        Endring(
-                            status = EndringStatus.valueOf("AVSLUTTET"),
-                            tidsstempel = LocalDateTime.parse("2024-08-14T11:51:16.176"),
-                            frist = null,
-                            endretAv = "Z994573"
-                        )
-                    )
-                ),
-                AvklaringsbehovHendelse(
-                    definisjon = Definisjon(
-                        type = "5097",
-                        behovType = BehovType.valueOf("MANUELT_PÅKREVD"),
-                        løsesISteg = "KVALITETSSIKRING"
-                    ),
-                    status = EndringStatus.valueOf("AVSLUTTET"),
-                    endringer = listOf(
-                        Endring(
-                            status = EndringStatus.valueOf("OPPRETTET"),
-                            tidsstempel = LocalDateTime.parse("2024-08-14T11:51:17.231"),
-                            frist = null,
-                            endretAv = "Kelvin"
-                        ),
-                        Endring(
-                            status = EndringStatus.valueOf("AVSLUTTET"),
-                            tidsstempel = LocalDateTime.parse("2024-08-14T11:54:22.268"),
-                            frist = null,
-                            endretAv = "Z994573"
-                        )
-                    )
-                )
-            ),
-            behandlingOpprettetTidspunkt = LocalDateTime.parse("2024-08-14T10:35:33.595")
-        )
-
+        val hendelse = behandlingHendelse(saksnummer, behandlingReferanse)
         val avsluttetBehandling = avsluttetBehandlingDTO(behandlingReferanse, saksnummer)
 
         val bqClient = BigQueryClient(config)
@@ -158,6 +62,11 @@ class IntegrationTest {
                 ventPåSvar({ bqClient.read(VilkårsVurderingTabell()) }, { t -> t.isNotEmpty() })
 
             assertThat(bigQueryRespons).hasSize(1)
+            val vilkårsVurderingRad = bigQueryRespons!!.first()
+
+            assertThat(vilkårsVurderingRad.vilkår.size).isEqualTo(avsluttetBehandling.vilkårsResultat.vilkår.size)
+            assertThat(vilkårsVurderingRad.behandlingsReferanse).isEqualTo(behandlingReferanse)
+            assertThat(vilkårsVurderingRad.saksnummer).isEqualTo(saksnummer)
         }
     }
 
--- app/src/test/kotlin/no/nav/aap/statistikk/testutils/testData.kt
+++ app/src/test/kotlin/no/nav/aap/statistikk/testutils/testData.kt
@@ -1,11 +1,18 @@
 package no.nav.aap.statistikk.testutils
 
+import no.nav.aap.statistikk.api_kontrakt.AvklaringsbehovHendelse
 import no.nav.aap.statistikk.api_kontrakt.AvsluttetBehandlingDTO
+import no.nav.aap.statistikk.api_kontrakt.BehovType
 import no.nav.aap.statistikk.api_kontrakt.BeregningsgrunnlagDTO
+import no.nav.aap.statistikk.api_kontrakt.Definisjon
+import no.nav.aap.statistikk.api_kontrakt.Endring
+import no.nav.aap.statistikk.api_kontrakt.EndringStatus
 import no.nav.aap.statistikk.api_kontrakt.Grunnlag11_19DTO
 import no.nav.aap.statistikk.api_kontrakt.GrunnlagYrkesskadeDTO
+import no.nav.aap.statistikk.api_kontrakt.MottaStatistikkDTO
 import no.nav.aap.statistikk.api_kontrakt.TilkjentYtelseDTO
 import no.nav.aap.statistikk.api_kontrakt.TilkjentYtelsePeriodeDTO
+import no.nav.aap.statistikk.api_kontrakt.TypeBehandling
 import no.nav.aap.statistikk.api_kontrakt.Utfall
 import no.nav.aap.statistikk.api_kontrakt.VilkårDTO
 import no.nav.aap.statistikk.api_kontrakt.VilkårsPeriodeDTO
@@ -13,6 +20,7 @@ import no.nav.aap.statistikk.api_kontrakt.VilkårsResultatDTO
 import no.nav.aap.statistikk.api_kontrakt.Vilkårtype
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
 
@@ -75,5 +83,84 @@ fun avsluttetBehandlingDTO(referanse: UUID, saksnummer: String): AvsluttetBehand
                 grunnlagEtterYrkesskadeFordel = BigDecimal(25000)
             ),
         )
+    )
+}
+
+fun behandlingHendelse(saksnummer: String, behandlingReferanse: UUID): MottaStatistikkDTO {
+    return MottaStatistikkDTO(
+        saksnummer = saksnummer,
+        behandlingReferanse = behandlingReferanse,
+        status = "UTREDES",
+        behandlingType = TypeBehandling.Førstegangsbehandling,
+        ident = "14890097570",
+        avklaringsbehov = listOf(
+            AvklaringsbehovHendelse(
+                definisjon = Definisjon(
+                    type = "5003",
+                    behovType = BehovType.valueOf("MANUELT_PÅKREVD"),
+                    løsesISteg = "AVKLAR_SYKDOM"
+                ),
+                status = EndringStatus.valueOf("SENDT_TILBAKE_FRA_KVALITETSSIKRER"),
+                endringer = listOf(
+                    Endring(
+                        status = EndringStatus.valueOf("OPPRETTET"),
+                        tidsstempel = LocalDateTime.parse("2024-08-14T10:35:34.842"),
+                        frist = null,
+                        endretAv = "Kelvin"
+                    ),
+                    Endring(
+                        status = EndringStatus.valueOf("AVSLUTTET"),
+                        tidsstempel = LocalDateTime.parse("2024-08-14T11:50:50.217"),
+                        frist = null,
+                        endretAv = "Z994573"
+                    )
+                )
+            ),
+            AvklaringsbehovHendelse(
+                definisjon = Definisjon(
+                    type = "5006",
+                    behovType = BehovType.valueOf("MANUELT_PÅKREVD"),
+                    løsesISteg = "VURDER_BISTANDSBEHOV"
+                ),
+                status = EndringStatus.valueOf("SENDT_TILBAKE_FRA_KVALITETSSIKRER"),
+                endringer = listOf(
+                    Endring(
+                        status = EndringStatus.valueOf("OPPRETTET"),
+                        tidsstempel = LocalDateTime.parse("2024-08-14T11:50:52.049"),
+                        frist = null,
+                        endretAv = "Kelvin"
+                    ),
+                    Endring(
+                        status = EndringStatus.valueOf("AVSLUTTET"),
+                        tidsstempel = LocalDateTime.parse("2024-08-14T11:51:16.176"),
+                        frist = null,
+                        endretAv = "Z994573"
+                    )
+                )
+            ),
+            AvklaringsbehovHendelse(
+                definisjon = Definisjon(
+                    type = "5097",
+                    behovType = BehovType.valueOf("MANUELT_PÅKREVD"),
+                    løsesISteg = "KVALITETSSIKRING"
+                ),
+                status = EndringStatus.valueOf("AVSLUTTET"),
+                endringer = listOf(
+                    Endring(
+                        status = EndringStatus.valueOf("OPPRETTET"),
+                        tidsstempel = LocalDateTime.parse("2024-08-14T11:51:17.231"),
+                        frist = null,
+                        endretAv = "Kelvin"
+                    ),
+                    Endring(
+                        status = EndringStatus.valueOf("AVSLUTTET"),
+                        tidsstempel = LocalDateTime.parse("2024-08-14T11:54:22.268"),
+                        frist = null,
+                        endretAv = "Z994573"
+                    )
+                )
+            )
+        ),
+        behandlingOpprettetTidspunkt = LocalDateTime.parse("2024-08-14T10:35:33.595")
     )
 }
