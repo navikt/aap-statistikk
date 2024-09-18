a6ea0416ab54a39930c24c78c65c6b9c0f706686
Fredrik Meyer <fredrik.meyer@nav.no>
2024-09-18 16:25:48 +0200

    Bedre feil-logging på insert i BigQuery

--- app/src/main/kotlin/no/nav/aap/statistikk/bigquery/BQRepository.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/bigquery/BQRepository.kt
@@ -4,26 +4,32 @@ import no.nav.aap.statistikk.avsluttetbehandling.IBeregningsGrunnlag
 import no.nav.aap.statistikk.avsluttetbehandling.MedBehandlingsreferanse
 import no.nav.aap.statistikk.tilkjentytelse.TilkjentYtelse
 import no.nav.aap.statistikk.vilkårsresultat.Vilkårsresultat
+import org.slf4j.LoggerFactory
 import java.util.UUID
+
+private val logger = LoggerFactory.getLogger(BQRepository::class.java)
 
 class BQRepository(
     private val client: BigQueryClient
-): IBQRepository {
+) : IBQRepository {
     private val vilkårsVurderingTabell = VilkårsVurderingTabell()
     private val tilkjentYtelseTabell = TilkjentYtelseTabell()
     private val beregningsGrunnlagTabell = BeregningsGrunnlagTabell()
 
     override fun lagre(payload: Vilkårsresultat) {
+        logger.info("Lagrer vilkårsresultat.")
         client.create(vilkårsVurderingTabell)
         client.insert(vilkårsVurderingTabell, payload)
     }
 
     override fun lagre(payload: TilkjentYtelse) {
+        logger.info("Lagrer tilkjent ytelse.")
         client.create(tilkjentYtelseTabell)
         client.insert(tilkjentYtelseTabell, payload)
     }
 
     override fun lagre(payload: IBeregningsGrunnlag, behandlingsReferanse: UUID) {
+        logger.info("Lagrer beregningsgrunnlag.")
         client.create(beregningsGrunnlagTabell)
         client.insert(
             beregningsGrunnlagTabell,
