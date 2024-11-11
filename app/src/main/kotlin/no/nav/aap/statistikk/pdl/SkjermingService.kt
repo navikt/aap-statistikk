fb6d04c2e6e42bc61e49bd275b84b2041add13b7
Fredrik Meyer <fredrik.meyer@nav.no>
2024-11-11 17:19:43 +0100

    Håndter feilet kall til PDL

--- app/src/main/kotlin/no/nav/aap/statistikk/pdl/SkjermingService.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/pdl/SkjermingService.kt
@@ -2,17 +2,22 @@ package no.nav.aap.statistikk.pdl
 
 import no.nav.aap.statistikk.behandling.Behandling
 
+private val logger = org.slf4j.LoggerFactory.getLogger("SkjermingService")
+
 class SkjermingService(
     private val pdlClient: PdlClient
 ) {
-
     fun erSkjermet(behandling: Behandling): Boolean {
         val identer = listOf(behandling.sak.person.ident) + behandling.relaterteIdenter
-        val hentPersoner = pdlClient.hentPersoner(identer)
-
-        return hentPersoner
-            .flatMap { it.adressebeskyttelse }
-            .any { it.gradering.erHemmelig() }
+        try {
+            val hentPersoner = pdlClient.hentPersoner(identer)
+            return hentPersoner
+                .flatMap { it.adressebeskyttelse }
+                .any { it.gradering.erHemmelig() }
+        } catch (e: Exception) {
+            logger.warn("Feilet kall til PDL. Returnerer false for skjerming. Se stackTrace.", e)
+            return false;
+        }
     }
 }
 
