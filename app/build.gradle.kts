daf7a29e4ad9965779d84ef00b5f249ee005d242
Fredrik Meyer <fredrik.meyer@nav.no>
2024-11-21 13:43:22 +0100

    Bump komponenter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val ktorVersion = "3.0.1"
 val mockkVersion = "1.13.13"
-val komponenterVersjon = "1.0.63"
+val komponenterVersjon = "1.0.67"
 val behandlingsflytversjon = "0.0.50"
 
 application {
