aeb0a2e3e6b8b35cbcab534a8a20064b19026207
Fredrik Meyer <fredrik.meyer@nav.no>
2024-11-27 09:19:40 +0100

    Bump behandlingsflyt igjen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 val ktorVersion = "3.0.1"
 val mockkVersion = "1.13.13"
 val komponenterVersjon = "1.0.70"
-val behandlingsflytversjon = "0.0.60"
+val behandlingsflytversjon = "0.0.61"
 
 application {
     mainClass.set("no.nav.aap.statistikk.AppKt")
