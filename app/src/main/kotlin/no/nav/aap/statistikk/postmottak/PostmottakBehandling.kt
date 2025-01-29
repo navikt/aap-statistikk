7817f525334f9be5195c1d3006f63ffa896b773a
Fredrik Meyer <fredrik.meyer@nav.no>
2025-01-29 16:43:59 +0100

    Bruk litt lavere tidsoppløsning i db

--- app/src/main/kotlin/no/nav/aap/statistikk/postmottak/PostmottakBehandling.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/postmottak/PostmottakBehandling.kt
@@ -4,6 +4,7 @@ import no.nav.aap.statistikk.behandling.KildeSystem
 import no.nav.aap.statistikk.behandling.TypeBehandling
 import no.nav.aap.statistikk.person.Person
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 class PostmottakBehandling(
@@ -51,7 +52,7 @@ class PostmottakBehandling(
         if (person != other.person) return false
         if (referanse != other.referanse) return false
         if (behandlingType != other.behandlingType) return false
-        if (mottattTid != other.mottattTid) return false
+        if (mottattTid.truncatedTo(ChronoUnit.MILLIS) != other.mottattTid.truncatedTo(ChronoUnit.MILLIS)) return false
         if (endringer != other.endringer) return false
 
         return true
@@ -62,7 +63,7 @@ class PostmottakBehandling(
         result = 31 * result + person.hashCode()
         result = 31 * result + referanse.hashCode()
         result = 31 * result + behandlingType.hashCode()
-        result = 31 * result + mottattTid.hashCode()
+        result = 31 * result + mottattTid.truncatedTo(ChronoUnit.MILLIS).hashCode()
         result = 31 * result + endringer.hashCode()
         return result
     }
@@ -72,10 +73,38 @@ class PostmottakBehandling(
     }
 }
 
-data class PostmottakOppdatering(
+class PostmottakOppdatering(
     val gjeldende: Boolean,
     val status: String,
     val oppdatertTid: LocalDateTime,
     val sisteSaksbehandler: String?,
     val gjeldendeAvklaringsBehov: String?,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PostmottakOppdatering
+
+        if (gjeldende != other.gjeldende) return false
+        if (status != other.status) return false
+        if (oppdatertTid.truncatedTo(ChronoUnit.MILLIS) != other.oppdatertTid.truncatedTo(ChronoUnit.MILLIS)) return false
+        if (sisteSaksbehandler != other.sisteSaksbehandler) return false
+        if (gjeldendeAvklaringsBehov != other.gjeldendeAvklaringsBehov) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = gjeldende.hashCode()
+        result = 31 * result + status.hashCode()
+        result = 31 * result + oppdatertTid.truncatedTo(ChronoUnit.MILLIS).hashCode()
+        result = 31 * result + (sisteSaksbehandler?.hashCode() ?: 0)
+        result = 31 * result + (gjeldendeAvklaringsBehov?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "PostmottakOppdatering(gjeldende=$gjeldende, status='$status', oppdatertTid=$oppdatertTid, sisteSaksbehandler=$sisteSaksbehandler, gjeldendeAvklaringsBehov=$gjeldendeAvklaringsBehov)"
+    }
+}
