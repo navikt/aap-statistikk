1ce15d31aa66e20160541f3111981fa3e27a5f07
Fredrik Meyer <fredrik.meyer@nav.no>
2025-02-20 11:13:08 +0100

    Optimaliser en sql-spørring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 val ktorVersion = "3.1.0"
 val mockkVersion = "1.13.16"
 val komponenterVersjon = "1.0.146"
-val behandlingsflytversjon = "0.0.159"
+val behandlingsflytversjon = "0.0.163"
 val tilgangVersjon = "1.0.4"
 val flywayVersion = "11.3.3"
 val oppgaveVersjon = "0.0.48"
