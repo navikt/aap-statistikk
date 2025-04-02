740687496647dd5e51aca09adf864a7065bf4727
Fredrik Meyer <fredrik.meyer@nav.no>
2025-04-02 11:03:31 +0200

    Fortsette å skille SakStatistikkService fra hendelsesService

---
+++ app/src/main/kotlin/no/nav/aap/statistikk/bigquery/LagreSakinfoTilBigQueryJobb.kt
@@ -0,0 +1,48 @@
+package no.nav.aap.statistikk.bigquery
+
+import no.nav.aap.komponenter.dbconnect.DBConnection
+import no.nav.aap.motor.Jobb
+import no.nav.aap.motor.JobbInput
+import no.nav.aap.motor.JobbUtfører
+import no.nav.aap.statistikk.behandling.IBehandlingRepository
+import no.nav.aap.statistikk.hendelser.SaksStatistikkService
+import no.nav.aap.statistikk.pdl.SkjermingService
+import no.nav.aap.statistikk.sak.IBigQueryKvitteringRepository
+
+class LagreSakinfoTilBigQueryJobbUtfører(private val sakStatistikkService: SaksStatistikkService) :
+    JobbUtfører {
+    override fun utfør(input: JobbInput) {
+        TODO("Not yet implemented")
+    }
+}
+
+class LagreSakinfoTilBigQueryJobb(
+    private val bigQueryKvitteringRepository: (DBConnection) -> IBigQueryKvitteringRepository,
+    private val behandlingRepositoryFactory: (DBConnection) -> IBehandlingRepository,
+    private val bqSakstatikk: IBQSakstatistikkRepository,
+    private val skjermingService: SkjermingService,
+) : Jobb {
+    override fun beskrivelse(): String {
+        return "Lagrer sakinfo til BigQuery"
+    }
+
+    override fun konstruer(connection: DBConnection): JobbUtfører {
+        val sakStatistikkService = SaksStatistikkService(
+            behandlingRepository = behandlingRepositoryFactory(connection),
+            bigQueryKvitteringRepository = bigQueryKvitteringRepository(connection),
+            bigQueryRepository = bqSakstatikk,
+            skjermingService = skjermingService,
+            rettighetstypeperiodeRepository = TODO(),
+            clock = TODO(),
+        )
+        return LagreSakinfoTilBigQueryJobbUtfører(sakStatistikkService)
+    }
+
+    override fun navn(): String {
+        return "lagreSakinfoTilBigQuery"
+    }
+
+    override fun type(): String {
+        return "statistikk.lagreSakinfoTilBigQueryJobb"
+    }
+}
