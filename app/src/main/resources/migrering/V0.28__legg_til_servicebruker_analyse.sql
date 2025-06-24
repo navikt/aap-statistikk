6f7f75af2a2b762593615551eae9bb8472fa15c6
Fredrik Meyer <fredrik.meyer@nav.no>
2025-06-24 08:40:10 +0200

    Legge til migrering i test-scope

--- app/src/main/resources/migrering/V0.28__legg_til_servicebruker_analyse.sql
+++ app/src/main/resources/migrering/V0.28__legg_til_servicebruker_analyse.sql
@@ -1,3 +1,6 @@
+-- PS, hopper spesifikt over versjon 0.27, for den ligger i test-scope for å opprette
+-- cloudsqliamserviceaccount-rollen, som allerede finnes i GCP.
+
 GRANT
 SELECT
     ON ALL TABLES IN SCHEMA public to cloudsqliamserviceaccount;
