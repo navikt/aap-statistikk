b3bde9f45463d35bbb2eec320e5e15bf6cc29983
Ine Lofthus Arnesen <ine.lofthus.arnesen@nav.no>
2025-09-10 08:01:41 +0200

    Legg til vilkår og vurderingsbehov for overgang arbeid og uføre

--- app/src/main/kotlin/no/nav/aap/statistikk/hendelser/vilkårsResultatTilDomene.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/hendelser/vilkårsResultatTilDomene.kt
@@ -54,5 +54,7 @@ private fun no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vilkårtype.tilDomene
         no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vilkårtype.SYKEPENGEERSTATNING -> Vilkårtype.SYKEPENGEERSTATNING
         no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vilkårtype.LOVVALG -> Vilkårtype.LOVVALG
         no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vilkårtype.SAMORDNING -> Vilkårtype.SAMORDNING
+        no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vilkårtype.OVERGANGARBEIDVILKÅRET -> Vilkårtype.OVERGANGARBEIDVILKÅRET
+        no.nav.aap.behandlingsflyt.kontrakt.statistikk.Vilkårtype.OVERGANGUFØREVILKÅRET -> Vilkårtype.OVERGANGUFØREVILKÅRET
     }
 }
