8dac73c3f5eb6e192f38039369315d457ff209a3
Fredrik Meyer <fredrik.meyer@nav.no>
2025-09-12 07:56:53 +0200

    Fjern et par avhengigheter som kommer via felleskomponenter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,6 @@ tasks.register<JavaExec>("genererOpenApi") {
 
 dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-core:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty:$ktorVersion")
 
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
     implementation("io.ktor:ktor-server-html-builder:${ktorVersion}")
