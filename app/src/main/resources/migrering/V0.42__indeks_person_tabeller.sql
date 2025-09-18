77cc6416e23dd3eaa9f0a53ceee742731987b685
Fredrik Meyer <fredrik.meyer@nav.no>
2025-09-18 09:21:14 +0200

    Unique index litt aggressivt

--- app/src/main/resources/migrering/V0.42__indeks_person_tabeller.sql
+++ app/src/main/resources/migrering/V0.42__indeks_person_tabeller.sql
@@ -1,3 +1,3 @@
 CREATE UNIQUE INDEX idx_person_id ON person (id);
 CREATE UNIQUE INDEX idx_relaterte_personer_id ON relaterte_personer (id);
-CREATE UNIQUE INDEX idx_relaterte_personer_person_id ON relaterte_personer (person_id);
+CREATE INDEX idx_relaterte_personer_person_id ON relaterte_personer (person_id);
