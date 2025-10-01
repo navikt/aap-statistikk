741f02b851cb47a76f738a7c87e91ed1bf7f914c
Fredrik Meyer <fredrik.meyer@nav.no>
2025-10-01 13:20:48 +0200

    Logg hele hendelsen i dev

--- app/src/main/kotlin/no/nav/aap/statistikk/jobber/LagreAvklaringsbehovHendelseJobbUtfører.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/jobber/LagreAvklaringsbehovHendelseJobbUtfører.kt
@@ -2,6 +2,7 @@ package no.nav.aap.statistikk.jobber
 
 import no.nav.aap.behandlingsflyt.kontrakt.statistikk.StoppetBehandling
 import no.nav.aap.komponenter.json.DefaultJsonMapper
+import no.nav.aap.komponenter.miljo.Miljø
 import no.nav.aap.motor.JobbInput
 import no.nav.aap.motor.JobbUtfører
 import no.nav.aap.statistikk.hendelser.HendelsesService
@@ -15,6 +16,10 @@ class LagreAvklaringsbehovHendelseJobbUtfører(private val hendelsesService: Hen
         val dto = DefaultJsonMapper.fromJson<StoppetBehandling>(input.payload())
         logger.info("StoppetBehandling mottatt Regenerer statistikk. Behandlingsreferanse: ${dto.behandlingReferanse}.")
 
+        if (!Miljø.erProd()) {
+            val hendelseJSON = DefaultJsonMapper.toJson(dto)
+            logger.info("Regenerer statistikk for behandling ${dto.behandlingReferanse}. Hendelse: $hendelseJSON")
+        }
         hendelsesService.prosesserNyHistorikkHendelse(dto)
         logger.info("Ferdig å regenerere statistikk for behandling ${dto.behandlingReferanse}.")
     }
