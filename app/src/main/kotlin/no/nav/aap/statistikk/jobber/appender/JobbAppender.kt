c934648b67bbd0815056f970f90dc07b822439d8
Fredrik Meyer <fredrik.meyer@nav.no>
2026-02-16 15:22:25 +0100

    Retry ved null enhet (#692)
    * Retry ved null enhet
    
    * Kommentar fra PR
    
    * Færre retries i tester

--- app/src/main/kotlin/no/nav/aap/statistikk/jobber/appender/JobbAppender.kt
+++ app/src/main/kotlin/no/nav/aap/statistikk/jobber/appender/JobbAppender.kt
@@ -5,12 +5,17 @@ import no.nav.aap.komponenter.repository.RepositoryProvider
 import no.nav.aap.motor.JobbInput
 import no.nav.aap.statistikk.avsluttetbehandling.LagreAvsluttetBehandlingTilBigQueryJobb
 import no.nav.aap.statistikk.behandling.BehandlingId
+import java.time.LocalDateTime
 
 interface JobbAppender {
     fun leggTil(connection: DBConnection, jobb: JobbInput)
     fun leggTil(repositoryProvider: RepositoryProvider, jobb: JobbInput)
     fun leggTilLagreSakTilBigQueryJobb(
-        repositoryProvider: RepositoryProvider, behandlingId: BehandlingId, delayInSeconds: Long = 0
+        repositoryProvider: RepositoryProvider,
+        behandlingId: BehandlingId,
+        delayInSeconds: Long = 0,
+        enhetRetryCount: Int = 0,
+        originalHendelsestid: LocalDateTime? = null
     )
 
     fun leggTilLagreAvsluttetBehandlingTilBigQueryJobb(
